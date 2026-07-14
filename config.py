ca69faee3647c5b36efe116f7ca7947809016d16
DALVOR-Produccion <sistema.orientacion.edu@gmail.com>
2026-07-14 09:51:37 -0400

    Conectar aplicación a PostgreSQL

--- config.py
+++ config.py
@@ -2,8 +2,18 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+database_url = os.getenv("DATABASE_URL")
 
 class Config:
-    SECRET_KEY = "davo_clave_secreta_inicial_2026"
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "davo_clave_secreta_inicial_2026"
+    )
+
+    SQLALCHEMY_DATABASE_URI = (
+        database_url
+        if database_url
+        else f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
